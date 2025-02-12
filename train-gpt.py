6809da738d63ddcd8b02e20c43b78bd37ec9f9d3
omanaokar <omanaokar@gmail.com>
2025-02-12 12:33:10 +0530

    Initialized mlp block and forward pass

--- train-gpt.py
+++ train-gpt.py
@@ -4,6 +4,20 @@ import torch.nn as nn
 from torch.nn import functional as f
 
 
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
 class Block(nn.Module):
 
     def __init__(self, config):
