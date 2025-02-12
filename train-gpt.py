7da794c46bf34a9a13705f08127287762f5acf39
omanaokar <omanaokar@gmail.com>
2025-02-12 17:37:50 +0530

    Updated config parameters according to 124M version

--- train-gpt.py
+++ train-gpt.py
@@ -69,11 +69,11 @@ class Block(nn.Module):
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
-    n_embd: int = 384
+    block_size: int = 1024 # max sequence length
+    vocab_size: int = 50257 # number of length: 50,000 BPE merges + 256 bytes tokens + 1 <|endofthestring
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of heads
+    n_embd: int = 768 # embedding dimension
 
 class GPT(nn.Module):
 
