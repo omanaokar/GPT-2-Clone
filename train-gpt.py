eeebce2554e900844a2e7a5f4ff921ea877d0236
omanaokar <omanaokar@gmail.com>
2025-02-12 18:45:04 +0530

    Sampling from the model

--- train-gpt.py
+++ train-gpt.py
@@ -165,7 +165,27 @@ model.to('cuda')
 # prefix tokens
 import tiktoken
 enc = tiktoken.get_encoding()
-tokens = enc.encode("Hello, I'm a language model")
+tokens = enc.encode("Hello, I'm a language model,")
 tokens = torch.tensor(tokens, dtype=torch.long) #(8,)
 tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8)
 x = tokens.to('cuda')
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length:
+    # forward the model to get logits
+    with torch.no_grad():
+        logits = model(x) # (B,T,vocab_size)
+        # take the logits at the last position
+        logits = logits[:, -1, :] # (B, vocab_size)
+        # get the probablities
+        probs = F.softmax(logits, dim=-1)
+        # do top-k sampling of 50 (huggingface pipeline default)
+        # topk_probs here becomes (5, 50), topk_indices is (5, 50)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        # select a token from the top-k probabilites
+        ix = torch.multinomial(topk_probs, 1) # (B,1)
+        # gather the corresponding indices
+        xcol = torch.gather(topk_indices, -1, ix) # (B,1)
+        # append to the sequence
+        x = torch.cat((x, xcol), dim=1)
